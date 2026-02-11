559ae6c294f433d780c3817b257840fedd776e55
Kholekile Somagaca <kholekile.s@gmail.com>
2026-02-11 16:59:12 +0200

    💚fix(build): update next config to basic

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["localhost"],
-    unoptimized: true,
-  },
-  output: "standalone",
+  reactStrictMode: true,
+  // Other configurations can be added here
 };
 
-export default nextConfig;
+module.exports = nextConfig;
